ed522858d16b54b300c982c49e33932de838493f
pein1625 <pein1625@gmail.com>
2019-11-22 23:14:28 +0700

    edit home css

--- dest/js/globals.js
+++ dest/js/globals.js
@@ -1,18 +1,16 @@
 // navbar mobile toggle
 $(function () {
-  var $body = $('html, body');
-  var $navbar = $('.js-navbar');
-  var $navbarOpen = $('.js-navbar-open');
-  var $navbarClose = $('.js-navbar-close');
+  var $body = $("html, body");
+  var $navbar = $(".js-navbar");
+  var $navbarOpen = $(".js-navbar-open");
+  var $navbarClose = $(".js-navbar-close");
 
-  $navbarOpen.on('click', function () {
-    $navbar.addClass('is-show');
-    $body.addClass('overflow-hidden');
+  $navbarOpen.on("click", function () {
+    $navbar.addClass("is-show");
   });
 
-  $navbarClose.on('click', function () {
-    $navbar.removeClass('is-show');
-    $body.removeClass('overflow-hidden');
+  $navbarClose.on("click", function () {
+    $navbar.removeClass("is-show");
   });
 });
 
@@ -26,6 +24,61 @@ $(function () {
     $toggle.parent().siblings('.menu-item-group').children('.menu-sub').slideUp();
 
     $toggle.parent().siblings('.menu-item-group').children('.menu-toggle').removeClass('active');
+  });
+});
+
+$(function () {
+  addSwiper(".banner-slider", {
+    speed: 1000,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    loop: true
+  });
+});
+
+// swiper template
+function addSwiper(selector, options = {}) {
+  return Array.from(document.querySelectorAll(selector), function (item) {
+    var $sliderContainer = $(item),
+        $sliderEl = $sliderContainer.find(selector + "__container");
+
+    if (options.navigation) {
+      $sliderContainer.addClass("has-nav");
+      options.navigation = {
+        prevEl: $sliderContainer.find(selector + "__prev"),
+        nextEl: $sliderContainer.find(selector + "__next")
+      };
+    }
+
+    if (options.pagination) {
+      $sliderContainer.addClass("has-pagination");
+      options.pagination = {
+        el: $sliderContainer.find(selector + "__pagination"),
+        clickable: true
+      };
+    }
+
+    return new Swiper($sliderEl, options);
+  });
+}
+
+$(function () {
+  $(".js-max-text").on("keydown", function (e) {
+    if ($(this).val().length >= 200) {
+      e.preventDefault();
+    }
+  });
+});
+
+$(function () {
+  $(".utilities__link").on("mouseenter click", function (e) {
+    e.preventDefault();
+    $(".utilities__link.active, .utilities__frame.active").removeClass("active");
+
+    $(this).addClass("active");
+    $($(this).attr("href")).addClass("active");
   });
 });
 
